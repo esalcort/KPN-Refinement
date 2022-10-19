5f51e44e8832d9e105c109aea63da650028b1b31
Erika Susana Alcorta <esalcort@utexas.edu>
2022-10-19 17:23:40 -0500

    OS solution

--- os_api.h
+++ os_api.h
@@ -13,6 +13,7 @@
  */
 
 #include <systemc.h>
+#include <queue>
 
 class   os_api : virtual public sc_interface
 {
@@ -28,16 +29,89 @@ class   os_api : virtual public sc_interface
 class OS: public virtual sc_channel, public os_api
 {
     /* Add your code in this class */
+    class Task {
+      public:
+        int id;
+        sc_event e;
+        const char* name;
+    };
+    Task* current_task = NULL;
+    int task_count = 0;
+    std::queue<Task*> rdyq;
+    Task * alivetasks[3];
+
 public:
-  OS(const sc_module_name name): sc_channel(name) { }
+  OS(const sc_module_name name): sc_channel(name) {}
 
-  void reg_task(const char* name) {  }
+  void dispatch() {
+    if (!rdyq.empty()){
+      current_task = rdyq.front();
+      rdyq.pop();
+      cout << "\t Dispatching " << current_task->name << endl;
+      cout << "\t\tnotify " << current_task->name << endl;
+      current_task->e.notify(SC_ZERO_TIME);
+    }
+    else {
+      current_task = NULL;
+    }
+  }
 
-  int pre_wait() { return 0; }
-  void post_wait(int task_id) { }
+  void yield() {
+    Task *t = current_task;
+    rdyq.push(t);
+    cout << "\t yield::pushing " << t->name << endl;
+    dispatch();
+    // if (current_task != t){
+      cout << "\t\twait " << t->name << endl;
+      wait(t->e);
+    // }
+  }
+
+  void reg_task(const char* name) {
+    Task *t = new Task();
+    alivetasks[task_count] = t;
+    t->id = task_count;
+    t->name = name;
+    task_count++;
+    if (current_task == NULL) {
+      current_task = t;
+      cout << "\t reg_task::current_task=" << current_task->name << endl;
+    }
+    else {
+      rdyq.push(t);
+      cout << "\t\twait " << t->name << endl;
+      wait(t->e);
+    }
+   }
+
+  int pre_wait() { 
+    Task *t = current_task;
+    cout << "\t pre_wait" << t->name << endl;
+    dispatch();
+    return t->id;
+  }
+  void post_wait(int task_id) {
+    Task *t = alivetasks[task_id];
+    rdyq.push(t);
+    cout << "\t post_wait::push" << t->name << endl;
+    if (current_task == NULL) {
+      dispatch();
+    }
+    // if (current_task != t) {
+      cout << "\t\twait " << t->name << endl;
+      wait(t->e);
+    // }
+  }
   
-  void time_wait(int time, sc_time_unit unit) {wait(time, unit); }
-  void task_terminate() { }
+  void time_wait(int time, sc_time_unit unit) {
+    wait(time, unit);
+    cout << "\t time_wait::yield" << endl;
+    yield();
+  }
+  void task_terminate() {
+    delete current_task;
+    dispatch();
+  }
   
 
 };
